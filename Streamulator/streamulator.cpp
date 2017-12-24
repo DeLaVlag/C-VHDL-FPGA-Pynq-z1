acf0355c5e916707c353e61d474a1ce281f883a1
GEBRUIK-AA5BGC3\ms <yoflippo@gmail.com>
2017-12-24 11:23:26 +0100

    small improvements.

--- Streamulator/streamulator.cpp
+++ Streamulator/streamulator.cpp
@@ -2,10 +2,10 @@
 #include <hls_opencv.h>
 
 ap_uint<32> pixeldata[HEIGHT][WIDTH];
+
 int main ()
 {
 	// Streams and data
-
 	hls::stream<pixel_data> inputStream;
 	hls::stream<pixel_data> outputStream;
 	pixel_data streamIn;
@@ -20,35 +20,34 @@ int main ()
 
 
 	// Write input data
-	for (int rows=0; rows < HEIGHT; rows++)
-		for (int cols=0; cols < WIDTH; cols++)
-		{
+	for (int rows=0; rows < HEIGHT; rows++){
+		for (int cols=0; cols < WIDTH; cols++){
 			streamIn.data = sourceImg.at<int>(rows,cols);
 			streamIn.user = (rows==0 && cols==0) ? 1 : 0;
 			streamIn.last = (cols==WIDTH-1) ? 1 : 0;
-
 			inputStream << streamIn;
 		}
+	}
 
 	// Call stream processing function
-	while (!inputStream.empty())
+	while (!inputStream.empty()){
 		stream(inputStream, outputStream);
+	}
 
 
 	// Read output data
-	for (int rows=0; rows < HEIGHT; rows++)
+	for (int rows=0; rows < HEIGHT; rows++){
 		for (int cols=0; cols < WIDTH; cols++)
 		{
 			outputStream.read(streamOut);
 			pixeldata[rows][cols] = streamOut.data;
 		}
-
+	}
 
 	// Save image by converting data array to matrix
 	// Depth or precision: CV_8UC4: 8 bit unsigned chars x 4 channels = 32 bit per pixel;
 	cv::Mat imgCvOut(cv::Size(WIDTH, HEIGHT), CV_8UC4, pixeldata);
 	cv::imwrite(OUTPUT_IMG, imgCvOut);
-
 
 	return 0;
 }
