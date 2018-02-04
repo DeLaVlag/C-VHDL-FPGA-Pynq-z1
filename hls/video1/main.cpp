0c1745238110a9f56057ade7b24135d2ca23c401
Michiel <m.a.van.der.vlag@hr.nl>
2018-02-04 11:45:59 +0100

    working version with load error though

--- hls/video1/main.cpp
+++ hls/video1/main.cpp
@@ -3,6 +3,7 @@
 void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uint8_t normalfactor, uint8_t channelselector)
 {
 #pragma HLS INTERFACE ap_ctrl_none port=return
+//#pragma HLS interface s_axilite port=return
 #pragma HLS INTERFACE axis port=&src
 #pragma HLS INTERFACE axis port=&dst
 #pragma HLS INTERFACE s_axilite port=kernelchc
@@ -13,42 +14,59 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 //Full unroll if factor = WIDTH*HEIGHT
 //#pragma HLS unroll factor=16
 
+<<<<<<< Updated upstream
 	short kernel[KERNEL_SIZE*KERNEL_SIZE] = {
+=======
+	uint8_t kernel[KERNEL_SIZE*KERNEL_SIZE] = {
+>>>>>>> Stashed changes
 							-1, -1, -1,
 							-1, 8, -1,
 							-1, -1, -1,
 	};
+<<<<<<< Updated upstream
 	short kernelEdge[KERNEL_SIZE*KERNEL_SIZE] = {
+=======
+	uint8_t kernelEdge[KERNEL_SIZE*KERNEL_SIZE] = {
+>>>>>>> Stashed changes
 							-1, -1, -1,
 							-1, 8, -1,
 							-1, -1, -1,
 	};
-	short kernelImpulse[KERNEL_SIZE*KERNEL_SIZE] = {
+	uint8_t kernelImpulse[KERNEL_SIZE*KERNEL_SIZE] = {
 							0, 0, 0,
 							0, 1, 0,
 							0, 0, 0,
 	};
-	short kernelBlur[KERNEL_SIZE*KERNEL_SIZE] = {
+	uint8_t kernelBlur[KERNEL_SIZE*KERNEL_SIZE] = {
 							1, 2, 1,
 							2, 4, 2,
 							1, 2, 1,
 	};
-	short kernelSobelY[KERNEL_SIZE*KERNEL_SIZE] = {
-							1, 2, 1,
-							0, 0, 0,
-							-1, -2, -1,
+	uint8_t kernelSobelY[KERNEL_SIZE*KERNEL_SIZE] = {
+								1, 2, 1,
+								0, 0, 0,
+								-1, -2, -1,
 	};
+<<<<<<< Updated upstream
 	short kernelSobelX[KERNEL_SIZE*KERNEL_SIZE] = {
 							-1, 0, 1,
 							-2, 0, 2,
 							-1, 0, 1,
 		};
+=======
+	uint8_t kernelSobelX[KERNEL_SIZE*KERNEL_SIZE] = {
+								-1, 0, 1,
+								-2, 0, 2,
+								-1, 0, 1,
+	};
+>>>>>>> Stashed changes
 
 //	uint8_t normalfactor=0;
 
 	pixel_data_in streamIn;
 	pixel_data_out streamOut;
 	linebuffer lb;
+<<<<<<< Updated upstream
 	linebuffer lb2;
 	window blurWin;
 	window gxWin, gyWin;
@@ -62,6 +80,28 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 	for (int pixels=0;pixels<HEIGHT*WIDTH;pixels++){
 #pragma HLS PIPELINE II=1
 //		dst.write(src.read());
+=======
+#pragma HLS RESOURCE variable=lb core=RAM_2P_BRAM
+//#pragma HLS ARRAY_PARTITION variable=lb cyclic factor=3
+#pragma HLS DEPENDENCE variable=lb array inter false
+
+linebuffer lb_gxy;
+#pragma HLS RESOURCE variable=lb_gxy core=RAM_2P_BRAM
+//#pragma HLS ARRAY_PARTITION variable=lb_gxy complete dim=1
+#pragma HLS DEPENDENCE variable=lb_gxy array inter false
+
+
+	window win;
+	window gxWin, gyWin;
+
+	uint16_t slidefactor=0;
+	uint16_t Gslidefactor=0;
+
+	uint16_t rows=0, cols=0;    //static adjusted
+
+	for (uint32_t pixels=0;pixels<HEIGHT*WIDTH;pixels++){
+#pragma HLS PIPELINE II=1
+>>>>>>> Stashed changes
 
 		streamIn = src.read();
 //		src >> streamIn;
@@ -74,7 +114,7 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 			//edge
 			case 0:
 			{
-				for (int i=0;i<KERNEL_SIZE*KERNEL_SIZE;i++)
+				for (uint8_t i=0;i<KERNEL_SIZE*KERNEL_SIZE;i++)
 					kernel[i] = kernelEdge[i];
 //				normalfactor=1;
 				break;
@@ -82,11 +122,12 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 			//impulse
 			case 1:
 			{
-				for (int i=0;i<KERNEL_SIZE*KERNEL_SIZE;i++)
+				for (uint8_t i=0;i<KERNEL_SIZE*KERNEL_SIZE;i++)
 					kernel[i] = kernelImpulse[i];
 //				normalfactor=4;
 				break;
 			}
+<<<<<<< Updated upstream
 
 			//blur
 			case 2:
@@ -110,6 +151,12 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 //				}
 
 				for (int i=0;i<KERNEL_SIZE*KERNEL_SIZE;i++)
+=======
+			//blur
+			case 2:
+			{
+				for (uint8_t i=0;i<KERNEL_SIZE*KERNEL_SIZE;i++)
+>>>>>>> Stashed changes
 					kernel[i] = kernelBlur[i];
 //				normalfactor=16;	//en.wikipedia.org/wiki/Kernel_(image_processing)
 
@@ -118,9 +165,14 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 			//sobel
 			case 3:
 			{
+<<<<<<< Updated upstream
 				for (int i=0;i<KERNEL_SIZE*KERNEL_SIZE;i++)
 					kernel[i] = kernelSobelY[i];
 //				normalfactor=1;
+=======
+				for (uint8_t i=0;i<KERNEL_SIZE*KERNEL_SIZE;i++)
+					kernel[i] = kernelSobelX[i];
+>>>>>>> Stashed changes
 				break;
 			}
 			case 4:
@@ -130,7 +182,6 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 				if (channelselector==2)streamOut.data = streamIn.data * 0x00000101;
 				if (channelselector==3)streamOut.data = streamIn.data * 0x00000001;
 				if (channelselector==4)streamOut.data = streamIn.data;
-//				streamOut.data = streamIn.data;
 				streamOut.keep = streamIn.keep;
 				streamOut.strb = streamIn.strb;
 				streamOut.user = streamIn.user;
@@ -142,7 +193,7 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 			}
 			default:
 			{
-				for (int i=0;i<KERNEL_SIZE*KERNEL_SIZE;i++)
+				for (uint8_t i=0;i<KERNEL_SIZE*KERNEL_SIZE;i++)
 					kernel[i] = kernelEdge[i];
 //				normalfactor=1;
 				break;
@@ -150,13 +201,22 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 
 		}
 
+<<<<<<< Updated upstream
 		if (kernelchoice!=4){
+=======
+		if (kernelchc!=4){
+
+			////////////////////////////////////////////////////////////
+			//Gaussian Blurring
+			////////////////////////////////////////////////////////////
+>>>>>>> Stashed changes
 
 			//LineBuffer shift down, while contents shift up (?!).
 			//Xilinx documentatie beweert het tegenovergestelde!
 			lb.shift_pixels_down(cols);
 			lb.insert_top_row(streamIn.data,cols);
 
+<<<<<<< Updated upstream
 
 			////////////////////////////////////////////////////////////
 			//Gaussian Blurring
@@ -171,10 +231,34 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 				currentPixelValue = pixelSummer(&blurWin);
 				currentPixelValue = currentPixelValue / normalfactor;
 				if (currentPixelValue < 0) currentPixelValue = 0;
+=======
+			convolution(&lb, slidefactor, kernel, &win, normalfactor);
 
+			// Stay within image boundaries and sum all pixel values in the window
+			uint8_t blurVal = 0;
+			if ((rows >= KERNEL_SIZE-1) && (cols >= KERNEL_SIZE-1))
+			{
+				// Convolution
+				blurVal = pixelSummer(&win);
+>>>>>>> Stashed changes
+
+				//normalizing for when kernel value is too big for datatype
+				//for edge kernel = 8 * 255 = 2040 < 2^16
+
+//				if (channelselector>1)
+//					blurVal = blurVal /channelselector;
+
+					// Stay positive
+				if (blurVal < 0)
+					blurVal = 0;
+
+				//increasing the iterator for sliding window over the linebuffers for kernelmult
 				slidefactor++;
-
 			}
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 
 			////////////////////////////////////////////////////////////
 			//Gradient calculation
@@ -190,6 +274,7 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 			convolution(&lb2, Gslidefactor, kernelSobelY, &gyWin);
 
 			//summing the results of both gradient conv and taking the hypot between both values
+<<<<<<< Updated upstream
 			char gxcpv, gycpv, gxycpv = 0;
 				if ((rows >= KERNEL_SIZE-1) && (cols >= KERNEL_SIZE-1)){
 					gxcpv = pixelSummer(&gxWin);
@@ -219,6 +304,28 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 //			if (channelselector==2)streamOut.data = gxycpv * 0x00000101;
 //			if (channelselector==3)streamOut.data = gxycpv;
 			streamOut.data = gxycpv;
+=======
+			uint8_t gxcpv, gycpv, gxycpv = 0;
+			if ((rows >= KERNEL_SIZE-1) && (cols >= KERNEL_SIZE-1)){
+				gxcpv = pixelSummer(&gxWin);
+				gycpv = pixelSummer(&gyWin);
+				if (gxcpv < 0) gxcpv = 0;
+				if (gycpv < 0) gycpv = 0;
+				gxycpv = (uint8_t)hypotf(gxcpv,gycpv);
+
+				Gslidefactor++;
+			}
+//			else
+//				gxycpv=0;
+
+
+			if (channelselector==0)streamOut.data = blurVal * 0x01010101;
+			if (channelselector==1)streamOut.data = gxycpv * 0x01010101;
+			if (channelselector==2)streamOut.data = blurVal;
+			if (channelselector==3)streamOut.data = gxycpv;
+//			if (channelselector==4)streamOut.data = blurVal;
+//			streamOut.data = blurVal;//* 0x01010101;
+>>>>>>> Stashed changes
 			streamOut.keep = streamIn.keep;
 			streamOut.strb = streamIn.strb;
 			streamOut.user = streamIn.user;
@@ -226,6 +333,7 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 			streamOut.id = streamIn.id;
 			streamOut.dest = streamIn.dest;
 			dst.write(streamOut);
+<<<<<<< Updated upstream
 	//		}
 
 			// Administration
@@ -237,10 +345,27 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 				}
 				else
 					cols++;
+=======
+
+
+			// Administration
+			if (streamIn.last){
+				cols = 0;
+				rows++;
+				slidefactor = 0;
+				Gslidefactor = 0;
+
+			}
+			else {
+				cols++;
+			}
+
+>>>>>>> Stashed changes
 		}
 
 	}
 }
+<<<<<<< Updated upstream
 
 
 void convolution(linebuffer *linebuffer, int slidefactor, short *kernel, window *win){
@@ -253,19 +378,38 @@ void convolution(linebuffer *linebuffer, int slidefactor, short *kernel, window 
 
 			// kernel * linebufcontent and place in a 3x3 window
 			val = (short)kernel[(wRows*KERNEL_SIZE) + wCols ] * val;
+=======
+
+void convolution(linebuffer *linebuffer, uint16_t slidefactor,
+		uint8_t *kernel, window *win, uint8_t normalfactor){
+//#pragma HLS ARRAY_PARTITION variable=linebuffer cyclic factor=2 dim=1 partition
+	// linebuffer values get multiplied by kernel and put in windowbuffer
+	for (uint8_t wRows = 0; wRows < KERNEL_SIZE; wRows++)
+		for (uint8_t wCols = 0; wCols < KERNEL_SIZE; wCols++)
+#pragma HLS PIPELINE rewind
+#pragma HLS unroll
+		{
+			// wCols + slidefactor, for sliding over buffer
+			uint8_t val = (uint8_t)linebuffer->getval(wRows,wCols+slidefactor);
+
+			// kernel * linebufcontent and place in a 3x3 window
+//#pragma HLS RESOURCE variable=val core=MulnS
+			val = ((uint8_t)kernel[(wRows*KERNEL_SIZE) + wCols ] * val)>>normalfactor;
+>>>>>>> Stashed changes
 			win->insert(val,wRows,wCols);
 		}
+//
 }
 
 // Convolution by adding all the values in the windows buffer
-short pixelSummer(hls::Window<KERNEL_SIZE,KERNEL_SIZE,short> *resultfromlinesliding)
+uint8_t pixelSummer(window *resultfromlinesliding)
 {
-	short sum = 0;
+	uint8_t sum = 0;
 
-	for (int rows = 0; rows < KERNEL_SIZE; rows++)
-		for (int cols = 0; cols < KERNEL_SIZE; cols++)
+	for (uint8_t rows = 0; rows < KERNEL_SIZE; rows++)
+		for (uint8_t cols = 0; cols < KERNEL_SIZE; cols++)
 		{
-			sum = sum + (short)resultfromlinesliding->getval(rows,cols);
+			sum = sum + (uint8_t)resultfromlinesliding->getval(rows,cols);
 		}
 	return sum;
 
