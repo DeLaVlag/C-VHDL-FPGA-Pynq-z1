6e822924b6172c3524e3814225f436022e4bd02e
Michiel <m.a.van.der.vlag@hr.nl>
2018-02-08 01:56:11 +0100

    working!

--- hls/video1/main.cpp
+++ hls/video1/main.cpp
@@ -18,7 +18,7 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 #pragma HLS RESOURCE variable=lb core=RAM_2P_BRAM
 #pragma HLS DEPENDENCE variable=lb array inter false
 
-	uint16_t rows=0, cols=0;    //static adjusted
+	static uint16_t rows=0, cols=0;    //static adjusted
 
 	for (uint32_t pixels=0;pixels<HEIGHT*WIDTH;pixels++){
 #pragma HLS PIPELINE II=1
@@ -28,50 +28,44 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 		lb.shift_pixels_down(cols);
 		lb.insert_top_row(streamIn,cols);
 
-		pixel_data_in blurVal;
-<<<<<<< Updated upstream
-		if ((rows >= KERNEL_SIZE-1) && (cols >= KERNEL_SIZE-1)){
-			if (kernelchc==0)blurVal = lb.getval(0,cols);
-			if (kernelchc==1)blurVal = lb.getval(1,cols);
-			if (kernelchc==2)blurVal = lb.getval(2,cols);
-			if (kernelchc==3)blurVal = lb.getval(3,cols);
-			if (kernelchc==4)blurVal = lb.getval(4,cols);
-		}
-		uint32_t outputPxl = (uint32_t)blurVal.data;
-		if (channelselector==0)streamOut.data = outputPxl * 0x01010101;
-		if (channelselector==1)streamOut.data = outputPxl * 0x00010101;
-		if (channelselector==2)streamOut.data = blurVal.data * 0x01010101;
-		if (channelselector==3)streamOut.data = blurVal.data;
-=======
+		uint8_t p1=0, p2=0, p3=0, p4=0;
 		uint32_t outputPxl=0;
 		uint32_t outputPxl2=0;
-		uint8_t outputPxl3=0;
+		uint32_t outputPxl3=0;
+		uint24 outPxl4=0, outPxl5=0, outPxl6=0;
 		if ((rows >= KERNEL_SIZE-1) && (cols >= KERNEL_SIZE-1)){
-			blurVal = lb.getval(2,cols);
-			outputPxl = blurVal.data;
-			outputPxl2 = outputPxl;
-			outputPxl2 = outputPxl2 - 100;
-			outputPxl3 = blurVal.data;;
-			if (outputPxl<0)outputPxl=0;
-			if (outputPxl2<0)outputPxl2=0;
-			if (outputPxl3<0)outputPxl3=0;
-		}
-		else {
-			blurVal.data = 0;
+			streamIn = lb.getval(2,cols);
+			outputPxl = streamIn.data;
+//			outputPxl2 = outputPxl;
+//			outputPxl2 = (outputPxl2 +255)/2;
+			outPxl4 = streamIn.data;;
+//			if (outputPxl<0)outputPxl=0;
+//			if (outputPxl2<0)outputPxl2=0;
+//			if (outputPxl3<0)outputPxl3=0;
+			p1=(outputPxl&0xFF);
+			p2=((outputPxl&0xFF00)>>8);
+			p3=((outputPxl&0xFF0000)>>16);
+			p4=((outputPxl&0xFF000000)>>24);
+
+			outputPxl2=(p4<<24|p3<<16|p2<<8|p1);
+			outputPxl3=(p3<<16|p2<<8|p1);
+			outPxl5=(p4<<24|p3<<16|p2<<8|p1);
+			outPxl6=(p3<<16|p2<<8|p1);
 		}
 
-		if (channelselector==0)streamOut.data = outputPxl * 0x00000001;
-		if (channelselector==1)streamOut.data = outputPxl * 0x00000100;
-		if (channelselector==2)streamOut.data = outputPxl * 0x00010000;;
-		if (channelselector==3)streamOut.data = blurVal.data * 0x01010101;
-		if (channelselector==4)streamOut.data = blurVal.data * 0x01000000;
-		if (channelselector==5)streamOut.data = (outputPxl2) * 0x01010101;
-		if (channelselector==6)streamOut.data = (outputPxl2) * 0x00010100;
-		if (channelselector==7)streamOut.data = outputPxl3 * 0x01010101;
-		if (channelselector==8)streamOut.data = outputPxl3;
-		if (channelselector==9)streamOut.data = (outputPxl3&0xFF)* 0x00000001;
 
->>>>>>> Stashed changes
+
+		if (channelselector==0)streamOut.data = outputPxl;
+		if (channelselector==1)streamOut.data = outputPxl2;
+		if (channelselector==2)streamOut.data = outputPxl3;
+		if (channelselector==3)streamOut.data = outPxl5;
+		if (channelselector==4)streamOut.data = outPxl6;
+		if (channelselector==5)streamOut.data = 0x4080C0FF<<normalfactor;
+		if (channelselector==6)streamOut.data = p1;
+		if (channelselector==7)streamOut.data = p2;
+		if (channelselector==8)streamOut.data = p3;
+		if (channelselector==9)streamOut.data = p4;
+
 		streamOut.keep = streamIn.keep;
 		streamOut.strb = streamIn.strb;
 		streamOut.user = streamIn.user;
