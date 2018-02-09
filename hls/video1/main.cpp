f14a9836998f55a642f82391ed265930913d312e
Michiel <m.a.van.der.vlag@hr.nl>
2018-02-09 09:26:24 +0100

    1st attempt own lb

--- hls/video1/main.cpp
+++ hls/video1/main.cpp
@@ -19,7 +19,33 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 	uint32_t outputPxl=0;
 	uint32_t outputPxl2=0;
 
-	linebuffer lb_p1, lb_p2, lb_p3, lb_p4;
+	uint8_t kernel[KERNEL_SIZE*KERNEL_SIZE] = {
+			0, 0, 0,
+			0, 1, 0,
+			0, 0, 0,
+		};
+
+	uint8_t lb_p1[3][WIDTH];
+	for(uint8_t i;i<3;i++)
+		for(uint16_t j;j<WIDTH;j++)
+			lb_p1[i][j]=0;
+
+	uint8_t lb_p2[3][WIDTH];
+	for(uint8_t i;i<3;i++)
+		for(uint16_t j;j<WIDTH;j++)
+			lb_p2[i][j]=0;
+
+	uint8_t lb_p3[3][WIDTH];
+	for(uint8_t i;i<3;i++)
+		for(uint16_t j;j<WIDTH;j++)
+			lb_p3[i][j]=0;
+
+	uint8_t lb_p4[3][WIDTH];
+	for(uint8_t i;i<3;i++)
+		for(uint16_t j;j<WIDTH;j++)
+			lb_p4[i][j]=0;
+
+//	linebuffer lb_p1, lb_p2, lb_p3, lb_p4;
 #pragma HLS RESOURCE variable=lb_p1 core=RAM_2P_BRAM
 #pragma HLS DEPENDENCE variable=lb_p1 array inter false
 #pragma HLS RESOURCE variable=lb_p2 core=RAM_2P_BRAM
@@ -43,23 +69,38 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 		p3=((pxlVal&0xFF0000)>>16);
 		p4=((pxlVal&0xFF000000)>>24);
 
+        if(kernelchc==0) for (uint8_t i=0; i<KERNEL_SIZE*KERNEL_SIZE; i++) kernel[i] = kernelImpulse[i];
+        if(kernelchc==1) for (uint8_t i=0; i<KERNEL_SIZE*KERNEL_SIZE; i++) kernel[i] = kernelBlur[i];
+        if(kernelchc==2) for (uint8_t i=0; i<KERNEL_SIZE*KERNEL_SIZE; i++) kernel[i] = kernelSobelY[i];
+        if(kernelchc==3) for (uint8_t i=0; i<KERNEL_SIZE*KERNEL_SIZE; i++) kernel[i] = kernelSobelX[i];
+        if(kernelchc==4) for (uint8_t i=0; i<KERNEL_SIZE*KERNEL_SIZE; i++) kernel[i] = kernelEdge[i];
+
 		////////////////////////////////////////////////////////////
 		//Gaussian Blurring
 		////////////////////////////////////////////////////////////
 
-		lb_p1.shift_pixels_down(cols);
-		lb_p1.insert_top_row(p1,cols);
-		lb_p2.shift_pixels_down(cols);
-		lb_p2.insert_top_row(p2,cols);
-		lb_p3.shift_pixels_down(cols);
-		lb_p3.insert_top_row(p3,cols);
-		lb_p4.shift_pixels_down(cols);
-		lb_p4.insert_top_row(p4,cols);
+        shiftPxlsDown(lb_p1, cols);
+        insertTop(lb_p1, cols,p1);
+        shiftPxlsDown(lb_p2, cols);
+        insertTop(lb_p2, cols,p2);
+        shiftPxlsDown(lb_p3, cols);
+        insertTop(lb_p3, cols,p3);
+        shiftPxlsDown(lb_p4, cols);
+        insertTop(lb_p4, cols,p4);
 
-		bVal1 = gaussianBlurring(rows, cols, &bWin1, &lb_p1, slidefactor, kernelBlur, normalfactor);
-		bVal2 = gaussianBlurring(rows, cols, &bWin2, &lb_p2, slidefactor, kernelBlur, normalfactor);
-		bVal3 = gaussianBlurring(rows, cols, &bWin3, &lb_p3, slidefactor, kernelBlur, normalfactor);
-		bVal4 = gaussianBlurring(rows, cols, &bWin4, &lb_p4, slidefactor, kernelBlur, normalfactor);
+//		lb_p1.shift_pixels_down(cols);
+//		lb_p1.insert_top_row(p1,cols);
+//		lb_p2.shift_pixels_down(cols);
+//		lb_p2.insert_top_row(p2,cols);
+//		lb_p3.shift_pixels_down(cols);
+//		lb_p3.insert_top_row(p3,cols);
+//		lb_p4.shift_pixels_down(cols);
+//		lb_p4.insert_top_row(p4,cols);
+
+		bVal1 = gaussianBlurring(rows, cols, &bWin1, lb_p1, slidefactor, kernel, normalfactor);
+		bVal2 = gaussianBlurring(rows, cols, &bWin2, lb_p2, slidefactor, kernel, normalfactor);
+		bVal3 = gaussianBlurring(rows, cols, &bWin3, lb_p3, slidefactor, kernel, normalfactor);
+		bVal4 = gaussianBlurring(rows, cols, &bWin4, lb_p4, slidefactor, kernel, normalfactor);
 
 		if ((rows >= KERNEL_SIZE-1) && (cols >= KERNEL_SIZE-1)){
 			slidefactor++;
@@ -90,7 +131,7 @@ void stream( pixel_stream_in &src, pixel_stream_out &dst, uint8_t kernelchc, uin
 		}
 	}
 }
-uint8_t gaussianBlurring(uint16_t rows, uint16_t cols, window *bWin, linebuffer *lb,
+uint8_t gaussianBlurring(uint16_t rows, uint16_t cols, window *bWin, uint8_t lb[KERNEL_SIZE][WIDTH],
 		uint16_t slifac, uint8_t *kernel, uint8_t nfac){
     uint8_t blurVal =0;
 
@@ -105,19 +146,35 @@ uint8_t gaussianBlurring(uint16_t rows, uint16_t cols, window *bWin, linebuffer 
     return blurVal;
 }
 
-void convolution(linebuffer *linebuffer, uint16_t slidefactor,
+void convolution(uint8_t val[KERNEL_SIZE][WIDTH], uint16_t slidefactor,
 uint8_t *kernel, window *win, uint8_t normalfactor){
+//#pragma HLS ARRAY_PARTITION variable=linebuffer cyclic factor=2 dim=1 partition
     for (uint8_t wRows = 0; wRows < KERNEL_SIZE; wRows++){
         for (uint8_t wCols = 0; wCols < KERNEL_SIZE; wCols++)
         {
             // wCols + slidefactor, for sliding over buffer
-            uint8_t val = (uint8_t)linebuffer->getval(wRows,wCols+slidefactor);
+            uint8_t pxl = (uint8_t)getval(val,wRows,wCols+slidefactor);
             // kernel * linebufcontent and place in a 3x3 window
-            val = ((uint8_t)kernel[(wRows*KERNEL_SIZE) + wCols ] * val)>>normalfactor;
-            win->insert(val,wRows,wCols);
+            pxl = ((uint8_t)kernel[(wRows*KERNEL_SIZE) + wCols ] * pxl)>>normalfactor;
+            win->insert(pxl,wRows,wCols);
         }
     }
 }
+
+//void convolution(linebuffer *linebuffer, uint16_t slidefactor,
+//uint8_t *kernel, window *win, uint8_t normalfactor){
+////#pragma HLS ARRAY_PARTITION variable=linebuffer cyclic factor=2 dim=1 partition
+//    for (uint8_t wRows = 0; wRows < KERNEL_SIZE; wRows++){
+//        for (uint8_t wCols = 0; wCols < KERNEL_SIZE; wCols++)
+//        {
+//            // wCols + slidefactor, for sliding over buffer
+//            uint8_t val = (uint8_t)linebuffer->getval(wRows,wCols+slidefactor);
+//            // kernel * linebufcontent and place in a 3x3 window
+//            val = ((uint8_t)kernel[(wRows*KERNEL_SIZE) + wCols ] * val)>>normalfactor;
+//            win->insert(val,wRows,wCols);
+//        }
+//    }
+//}
 
 // Convolution by adding all the values in the windows buffer
 uint8_t pixelSummer(window *resultfromlinesliding){
@@ -130,4 +187,20 @@ uint8_t pixelSummer(window *resultfromlinesliding){
 		}
 	}
 	return sum;
+}
+
+void shiftPxlsDown(uint8_t val[KERNEL_SIZE][WIDTH], uint16_t col){
+
+    for(uint8_t i = KERNEL_SIZE-1; i > 0; i--) {
+#pragma HLS unroll
+        val[i][WIDTH] = val[i-1][col];
+    }
+}
+
+void insertTop(uint8_t val[KERNEL_SIZE][WIDTH], uint16_t col, uint8_t value){
+	val[0][col] = value;
+}
+
+uint8_t getval(uint8_t val[KERNEL_SIZE][WIDTH], uint16_t row, uint16_t col){
+	return val[row][col];
 }
